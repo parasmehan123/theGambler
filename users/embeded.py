82ebd9d12152c98d1760062667e79a7393cb47d9
MehulDokania <43749533+MehulDokania@users.noreply.github.com>
2020-04-30 12:32:59 +0530

    Fixed bugs

--- users/embeded.py
+++ users/embeded.py
@@ -177,6 +177,61 @@ def update_records(email,won,bet,game_id):
         cursor.execute("select player_id from player_email where email like \"%s\"",[email])
         player_id = cursor.fetchall()[0][0]
         cursor.execute("insert into game_transaction(game_id,player_id,won_lost,bet,dt) values(%s,%s,%s,%s,NOW());",[game_id,player_id,1 if won else 0,1 if bet else 0])
+        
+        cursor.execute("update game_details set no_of_wins = no_of_wins + %s where player_id = %s AND game_id = %s ;",[1 if won else 0, player_id, game_id+3]  )
+        connection.commit()
+        cursor.execute("update game_details set no_of_losses = no_of_losses + %s where player_id = %s AND game_id = %s ;",[0 if won else 1, player_id, game_id+3]  )
+        connection.commit()
+        cursor.execute("update game_details set amount_won = amount_won + %s where player_id = %s AND game_id = %s ;",[100 if won else 0, player_id, game_id+3]  )
+        connection.commit()
+        cursor.execute("update game_details set amount_lost = amount_lost + %s where player_id = %s AND game_id = %s ;",[0 if won else 100, player_id, game_id+3]  )
+        connection.commit()
+
+        cursor.execute("update player_ranklist set no_of_wins = no_of_wins + %s where player_id = %s ;",[1 if won else 0, player_id]  )
+        connection.commit()
+        cursor.execute("update player_ranklist set no_of_loses = no_of_loses + %s where player_id = %s ;",[0 if won else 1, player_id]  )
+        connection.commit()
+        cursor.execute("update player_ranklist set total_profit = total_profit + %s where player_id = %s ;",[100 if won else 0, player_id]  )
+        connection.commit()
+        cursor.execute("update player_ranklist set total_loss = total_loss + %s where player_id = %s ;",[0 if won else 100, player_id]  )
+        connection.commit()
+
+        cursor.execute("update game set no_of_games_played = no_of_games_played + %s where id = %s ;", [1, game_id+3]  )
+        connection.commit()
+        cursor.execute("update game set total_profit = total_profit + %s where id = %s ;",[100 if won else 0, game_id+3]  )
+        connection.commit()
+        cursor.execute("update game set total_loss = total_loss + %s where id = %s ;",[0 if won else 100, game_id+3]  )
+        connection.commit()
+        
+        #Update ranklist in game_details
+        cursor.execute("alter table game_details drop column rank")
+        connection.commit()
+        cursor.execute("set @row_number = 0;")
+        cursor.execute("select game_id, player_id, @row_number := @row_number + 1 AS rank from game_details order by no_of_wins desc;")
+        data = cursor.fetchall()
+        # print(data)
+        cursor.execute("alter table game_details add Rank int not null")
+        connection.commit()
+        for x in data:
+            # print(x)
+            cursor.execute("update game_details set rank = " + str(x[2]) + " where player_id = " + str(x[1]) + " && game_id = " + str(x[0] + 3) + " ;")
+            connection.commit()
+
+
+        #Update ranklist in player_ranklist
+        cursor.execute("alter table player_ranklist drop column rank")
+        connection.commit()
+        cursor.execute("set @row_number = 0;")
+        cursor.execute("select player_id, @row_number := @row_number + 1 AS rank from player_ranklist order by no_of_wins desc;")
+        data = cursor.fetchall()
+        # print(data)
+        cursor.execute("alter table player_ranklist add Rank int not null")
+        connection.commit()
+        for x in data:
+            print(x)
+            cursor.execute("update player_ranklist set rank = " + str(x[1]) + " where player_id = " + str(x[0]) + " ;")
+            connection.commit()
+
         cursor.execute("select id from account where player_id = %s",[player_id])
         account_id = cursor.fetchall()[0][0]
         if bet:
@@ -253,7 +308,7 @@ def player_games_not_played(email):
     with connection.cursor() as cursor:
         cursor.execute("select player_id from player_email where email like \"%s\"",[email])
         player_id = cursor.fetchall()[0][0]
-        cursor.execute("SELECT name	FROM game WHERE game.id not in (SELECT game_id  FROM game_details WHERE player_id = %s);",[player_id])
+        cursor.execute("SELECT name	FROM game WHERE game.id not in (SELECT game_id  FROM game_details WHERE player_id = %s and (no_of_wins <> %s or no_of_losses <> %s) );",[player_id,0,0])
         columns_names = [col[0] for col in cursor.description]
         data_raw = cursor.fetchall()
 
@@ -313,7 +368,7 @@ def player_account_balance(email):
 
 def player_ranklist():
     with connection.cursor() as cursor:
-        cursor.execute("select rank,player_id,name from player_ranklist,player where player.id = player_ranklist.player_id;")
+        cursor.execute("select rank,player_id,name from player_ranklist,player where player.id = player_ranklist.player_id order by rank asc;")
         columns_names = [col[0] for col in cursor.description]
         data_raw = cursor.fetchall()
 
